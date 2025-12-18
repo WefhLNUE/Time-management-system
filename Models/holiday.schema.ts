9a741d2802e2846e78529f3c6a6453d61e3b1b9d
yousseffkhaledd <youssefkmamdouh@gmail.com>
2025-12-18 21:31:18 +0200

    restore model files & edit cron

--- Models/holiday.schema.ts
+++ Models/holiday.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-import { HolidayType } from './enums';
+import { HolidayType } from './enums/index';
 
 export type HolidayDocument = HydratedDocument<Holiday>;
 
@@ -13,7 +13,7 @@ export class Holiday {
   startDate: Date;
 
   @Prop()
-  endDate?: Date;
+  endDate?: Date; // if missing, startDate == holiday day
 
   @Prop()
   name?: string;
--- Models/notification-log.schema.ts
+++ Models/notification-log.schema.ts
@@ -1,21 +1,19 @@
-// src/time-management/Models/notification-log.schema.ts
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Types, HydratedDocument } from 'mongoose';
+import { Types } from 'mongoose';
+import { HydratedDocument } from 'mongoose';
 
 export type NotificationLogDocument = HydratedDocument<NotificationLog>;
 
 @Schema({ timestamps: true })
 export class NotificationLog {
-    @Prop({ type: Types.ObjectId, ref: 'EmployeeProfile', required: false })
-    to?: Types.ObjectId | null;
+    @Prop({type: Types.ObjectId, ref: 'EmployeeProfile', required: true})
+    to: Types.ObjectId;
 
     @Prop({ required: true })
     type: string;
 
     @Prop()
     message?: string;
-
 }
 
-export const NotificationLogSchema =
-    SchemaFactory.createForClass(NotificationLog);
+export const NotificationLogSchema = SchemaFactory.createForClass(NotificationLog);
